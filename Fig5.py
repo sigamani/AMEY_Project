669607151a14f87b492ab726d58d9dd6e1a975fa
Administrator <sigamani@cern.ch>
2016-06-18 18:25:01 +0100

    Small changes

--- Fig5.py
+++ Fig5.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import csv 
 import math
 
-with open('VehicleData.csv') as csvfile:
+with open('VehicleData.csv.orig') as csvfile:
 
 	readCSV = csv.reader(csvfile, delimiter=',') 
 
@@ -101,7 +101,9 @@ with open('VehicleData.csv') as csvfile:
 				mean10ScorePerYear.append(ConditionScore[pos-1])
 
 
-		# Calculate mean score per vehicle brand per year
+
+		# Calculate mean score per vehicle brand per year (control for exceptions)
+
 		mean1 = np.mean(mean1ScorePerYear)
 		std1 = np.std(mean1ScorePerYear,dtype=np.float64)
 		if (math.isnan(mean1)): 
@@ -183,26 +185,61 @@ with open('VehicleData.csv') as csvfile:
 		Y10err.append(std10)
 
 
+        #print "Mean (1) \t = %f, error = %f %%" %(mean1,std1/mean1*100)
+        #print "Mean (2) \t = %f, error = %f %%" %(mean2,std2/mean2*100)
+        #print "Mean (3) \t = %f, error = %f %%" %(mean3,std3/mean3*100)
+        #print "Mean (4) \t = %f, error = %f %%" %(mean4,std4/mean4*100)
+        #print "Mean (5) \t = %f, error = %f %%" %(mean5,std5/mean5*100)
+        #print "Mean (6) \t = %f, error = %f %%" %(mean6,std6/mean6*100)
+        #print "Mean (7) \t = %f, error = %f %%" %(mean7,std7/mean7*100)
+        #print "Mean (8) \t = %f, error = %f %%" %(mean8,std8/mean8*100)
+        #print "Mean (9) \t = %f, error = %f %%" %(mean9,std9/mean9*100)
+        #print "Mean (10)\t = %f, error = %f %%" %(mean10,std10/mean10*100)
+
+		
+m1 = np.mean(Y1)
+m2 = np.mean(Y2)
+m3 = np.mean(Y3)
+m4 = np.mean(Y4)
+m5 = np.mean(Y5)
+m6 = np.mean(Y6)
+m7 = np.mean(Y7)
+m8 = np.mean(Y8)
+m9 = np.mean(Y9)
+m10 = np.mean(Y10)
+
+print "1 = %f" %m1
+print "2 = %f" %m2
+print "3 = %f" %m3
+print "4 = %f" %m4
+print "5 = %f" %m5
+print "6 = %f" %m6
+print "7 = %f" %m7
+print "8 = %f" %m8
+print "9 = %f" %m9
+print "10 = %f" %m10
+		
 N = 12
 ind = np.arange(N)  # the x locations for the groups
-width = 0.08
+width = 0.08 
+shift = 0.08
 fig, ax = plt.subplots()
 
-rects1 = ax.bar(ind, Y1, width, color='lightblue', yerr=Y1err, ecolor='black')
-rects2 = ax.bar(ind+width, Y2, width, color='lightgreen', yerr=Y2err, ecolor='black')
-rects3 = ax.bar(ind+2*width, Y3, width, color='lightgrey', yerr=Y3err, ecolor='black')
-rects4 = ax.bar(ind+3*width, Y4, width, color='red', yerr=Y4err, ecolor='black')
-rects5 = ax.bar(ind+4*width, Y5, width, color='blue', yerr=Y5err, ecolor='black')
-rects6 = ax.bar(ind+5*width, Y6, width, color='purple', yerr=Y6err, ecolor='black')
-rects7 = ax.bar(ind+6*width, Y7, width, color='cyan', yerr=Y7err, ecolor='black')
-rects8 = ax.bar(ind+7*width, Y8, width, color='orange', yerr=Y8err, ecolor='black')
-rects9 = ax.bar(ind+8*width, Y9, width, color='green', yerr=Y9err, ecolor='black')
-rects10 = ax.bar(ind+9*width, Y10, width, color='brown', yerr=Y10err, ecolor='black')
+rects1 = ax.bar(ind+shift, Y1, width, color='lightgrey' )
+rects2 = ax.bar(ind+width+shift, Y2, width, color='blue' )
+rects3 = ax.bar(ind+2*width+shift, Y3, width, color='lightgreen' )
+rects4 = ax.bar(ind+3*width+shift, Y4, width, color='red' )
+rects5 = ax.bar(ind+4*width+shift, Y5, width, color='khaki' )
+rects6 = ax.bar(ind+5*width+shift, Y6, width, color='purple' )
+rects7 = ax.bar(ind+6*width+shift, Y7, width, color='cyan' )
+rects8 = ax.bar(ind+7*width+shift, Y8, width, color='orange' )
+rects9 = ax.bar(ind+8*width+shift, Y9, width, color='lemonchiffon' )
+rects10 = ax.bar(ind+9*width+shift, Y10, width, color='brown', )
 
 ax.set_xticks(ind + 0.5)
 ax.set_xticklabels(('2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012'))
 ax.legend((rects1[0], rects2[0], rects3[0], rects4[0], rects5[0], rects6[0], rects7[0], rects8[0], rects9[0], rects10[0]), 
-('Volkswagen', 'DAF', 'Iveco', 'Renault', 'Ford', 'MAN','Fiat', 'Volvo', 'Mercedes Benz', 'Scania'), loc=1,prop={'size':12}, bbox_to_anchor=(1.1, 1), borderaxespad=0.)
+('Volkswagen', 'DAF', 'Iveco', 'Renault', 'Ford', 'MAN','Fiat', 'Volvo', 'Mercedes Benz', 'Scania'), prop={'size':12}, bbox_to_anchor=(0.35, 1.1), loc=2, borderaxespad=0.)
 
 
 #Plot gridlines
